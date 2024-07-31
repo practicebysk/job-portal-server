3ce657e1029c6d6914514bdaa1270f4b0d7a23ad
sk <kinarsardhara225@gamil.com>
2024-07-30 19:21:56 -0500

    test

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -100,9 +100,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpsOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV === "production",
-        domain: 'https://job-portal-ng.netlify.app'
+        sameSite: "none",
+        secure: true
       })
       .json({
         message: `Welcome back ${user.fullname}`,
